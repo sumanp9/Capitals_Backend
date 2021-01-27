592244d0e5cdb74b5d559f0148577f9d26957d6f
Suman Pradhab <11.sumanpradhan@gmail.com>
2021-01-27 16:01:41 -0600

    Added a repository file for accessing country data

--- src/main/java/com/captials/app/repository/CountryRepository.java
+++ src/main/java/com/captials/app/repository/CountryRepository.java
@@ -2,6 +2,8 @@ package com.captials.app.repository;
 
 import com.captials.app.Country;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CountryRepository extends CrudRepository<Long, Country> {
 }
