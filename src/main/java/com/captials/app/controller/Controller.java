ceea097cb7c229ced4e5101e023c6a4fd638034a
Suman Pradhab <11.sumanpradhan@gmail.com>
2021-01-27 16:17:42 -0600

    Creating rest end points

--- src/main/java/com/captials/app/controller/Controller.java
+++ src/main/java/com/captials/app/controller/Controller.java
@@ -1,14 +1,26 @@
 package com.captials.app.controller;
 
+import com.captials.app.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    CountryService countryService;
+
     @GetMapping("/")
     public String home() {
         return "<h1>Hello and Welcome to the App</h1>";
+    }
+
+    @GetMapping("/getAllCountries")
+    public List<String> getAllCountries() {
+        return this.countryService.getAllCountries();
     }
 
 }
---
+++ src/main/java/com/captials/app/service/CountryService.java
@@ -0,0 +1,2 @@
+package com.captials.app.service;public class CountryService {
+}
