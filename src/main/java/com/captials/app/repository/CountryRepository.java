893c31b6b98bdc3d0f7ad832f17c0e9a36d75bc3
Suman Pradhab <11.sumanpradhan@gmail.com>
2021-01-29 12:43:39 -0600

    Added a custom query which returns the name of countries

--- src/main/java/com/captials/app/repository/CountryRepository.java
+++ src/main/java/com/captials/app/repository/CountryRepository.java
@@ -1,9 +1,14 @@
 package com.captials.app.repository;
 
 import com.captials.app.Country;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CountryRepository extends CrudRepository<Long, Country> {
+public interface CountryRepository extends CrudRepository<Country, Long> {
+    @Query(value = "SELECT name from Country")
+    List<String> findAllCountries();
 }
