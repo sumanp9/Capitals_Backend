74d42bc0cd9f4f77e7853a6c86c3fc6f519a7222
Suman Pradhab <11.sumanpradhan@gmail.com>
2021-01-29 12:47:06 -0600

    Writing business logic to get name of all countries

--- src/main/java/com/captials/app/service/CountryService.java
+++ src/main/java/com/captials/app/service/CountryService.java
@@ -1,2 +1,18 @@
-package com.captials.app.service;public class CountryService {
+package com.captials.app.service;
+
+import com.captials.app.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    CountryRepository repository;
+
+    public List<String> getAllCountries() {
+        return repository.findAllCountries();
+    }
 }
