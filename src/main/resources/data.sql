e1fee14a92fb9a87c2414cd59c764639794806fa
Alson <lsonshrestha@gmail.com>
2021-02-01 17:20:50 -0600

    Added south asian countries and sort by ascending order in data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,11 @@
 INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Nepal',50000,'Kathmandu','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Afghanistan',38928346,'Kabul','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Bangladesh',164689383,'Dhaka','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Bhutan',771608,'Thimpu','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('India',138004385,'New Delhi','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Maldives',540544,'Male','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Pakistan',220892340,'Islamabad','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Sri Lanka',21413249,'Colombo','South','Asia');
+INSERT INTO tbl_country (name, population, capital, region, continent) VALUES ('Iran',83992949,'Tehran','South','Asia');
+
+SELECT * FROM tbl_country ORDER BY name ASC;
